89dcb9d7a98720953b047d2f526e9c0584e6cbaf
Romanhessec <romangabimarian@gmail.com>
2024-12-27 16:05:34 +0200

    Rough skel for preprocessing pipeline

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # big-data-medical-analysis
 
 ## Dataset
-The dataset used in this project is CheXpert. You can download it from [this link](https://github.com/rajpurkarlab/cheXpert-test-set-labels).
+The dataset used in this project is CheXpert. You can download it from [this link](https://stanfordaimi.azurewebsites.net/datasets/23c56a0d-15de-405b-87c8-99c30138950c).
 
 After downloading, place the dataset in the root of this project directory.
--- utils/pkl_to_png_converter.py
+++ utils/pkl_to_png_converter.py
@@ -6,25 +6,25 @@ import pickle
 import matplotlib.pyplot as plt
 import torch
 
-# Load the .pkl file
-file_path = 'chexlocalize/CheXlocalize/gradcam_maps_val/patient64738_study1_view1_frontal_Consolidation_map.pkl'
+# load the .pkl file
+file_path = '../chexlocalize/CheXlocalize/gradcam_maps_val/patient64738_study1_view1_frontal_Consolidation_map.pkl'
 
 with open(file_path, 'rb') as file:
     data = pickle.load(file)
 
-# Extract the chest X-ray tensor ('cxr_img')
+# extract the chest X-ray tensor ('cxr_img')
 cxr_img = data['cxr_img']
 
-# Convert it to a NumPy array
+# convert it to a NumPy array
 if isinstance(cxr_img, torch.Tensor):
     cxr_img = cxr_img.numpy()
 
-# Plot the image
+# plot the image
 plt.imshow(cxr_img[0], cmap='gray')
 plt.title(f"Task: {data['task']} (Ground Truth: {data['gt']})")
 plt.axis('off')
 
-# Save the image to a file
+# save the image to a file
 output_path = 'output_image.png'
 plt.savefig(output_path)
 print(f"Image saved to {output_path}")
